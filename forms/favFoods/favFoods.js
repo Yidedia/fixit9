f2312907bf0d26ad2a59f8145c118dd4669de732
Yidedia <70745425+Yidedia@users.noreply.github.com>
2020-10-13 18:41:51 -0500

    drop down created
    it populates the list

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,9 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+ddf.onshow=function(){
+    // always clear before populating
+    ddf.clear()   
+    // put array of food in the dropdown (called populating it)
+    for (i = 0; i <= goodFoods.length - 1; i++) 
+        ddf.addItem(goodFoods[i])
+}
