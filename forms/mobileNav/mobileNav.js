20372defcdf5f19d994daf5cfc5adafb2744621a
Yidedia <70745425+Yidedia@users.noreply.github.com>
2020-10-14 15:05:19 -0500

    save done
    ds

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,8 +1,31 @@
-
-hmbMenu.onclick=function(s){
-  if (typeof(s) == "object") 
-    return
-  /* add a switch here to take the user to the form
+/* add a switch here to take the user to the form
      they chose. 
   */
+hmbMenu.onclick = function(s) {
+  if (typeof(s) == "object") {
+    return
+} else {
+  switch (s) {
+    case "Login":
+      // hide the hamberger menu and show the signin button
+      ChangeForm(loginCU)
+      break
+    case "Favorite Foods":
+      // Go to the Demo form
+      ChangeForm(favFoods)
+      break
+    case "Dessert Voting":
+      // add an item to the hamburger menu at runtime just for fun
+      ChangeForm(dessertVoting)
+      break
+    case "Describe Me":
+      // add an item to the hamburger menu at runtime just for fun
+      ChangeForm(describeYou)
+      break
+    case "Favorite Exercises":
+      // add an item to the hamburger menu at runtime just for fun
+      ChangeForm(favExercises)
+      break
+  } //switch
+} //else
 }
