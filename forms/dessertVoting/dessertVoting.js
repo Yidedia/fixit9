9ce162ed4da02baa8cd424a1ce9b401c11bf4643
Yidedia <70745425+Yidedia@users.noreply.github.com>
2020-10-13 19:40:35 -0500

    done wiht program 2

--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -1,22 +1,30 @@
-var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
-                  'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
+var dessertList = ['Chocolate Cake', 'Cream Brulee', 'Cheesecake',
+  'Keyline Pie', 'Cherry Cobbler', 'Chocolate Chip Cookies'
+]
 
-dessertVoting.onshow=function(){
-    drpDesserts.clear()   
+dessertVoting.onshow = function() {
+  drpDesserts.clear()
   /* add a loop here that adds all the desserts in the array 
      to the dropdown.
   */
+  for (i = 0; i <= dessertList.length - 1; i++)
+    drpDesserts.addItem(dessertList[i])
 }
 
 
-drpDesserts.onclick=function(){
-   // this 'if' kicks user out if they  just clicked on control 
- // but not on one item in the list.
-    if (typeof(s) == "object")   
-      return                    
-    else {  // the user picked something
-        /* add code here to get item user chose and output 
-        it in a label, using a literal.
-        */
-    }
+drpDesserts.onclick = function(s) {
+  // this 'if' kicks user out if they  just clicked on control 
+  // but not on one item in the list.
+  if (typeof(s) == "object")
+    return
+  else { // the user picked something
+    /* add code here to get item user chose and output 
+    it in a label, using a literal.
+    */
+    drpDesserts.value = s // make dropdown show the choice the user made
+    lblOutput.value = `You picked ${s} -that is a great choice!`
+  }
+}
+btnTODY.onclick=function(){
+  ChangeForm(describeYou)
 }
